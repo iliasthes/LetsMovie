3c3efdc64890e75a3d708f473cba26d3b4c2120b
iliasthes <iliasthes@hotmail.com>
2017-01-25 22:01:30 +0200

    25/1

--- app/src/main/java/com/example/android/letsmovie/MainActivity.java
+++ app/src/main/java/com/example/android/letsmovie/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.android.letsmovie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private RecyclerView mMoviesGrid;
+    private GridLayoutManager layoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
